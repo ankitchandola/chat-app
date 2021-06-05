b47a4f92a1f79cea0638ba30173ba5de8bf57858
ankit <sindeadly25@gmail.com>
2021-06-05 19:21:01 +0530

    firebase added

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCTRz3U7JVoHauNwsB_f9H6F5cr0SuO9Rk',
+  authDomain: 'chat-app-1c6f3.firebaseapp.com',
+  projectId: 'chat-app-1c6f3',
+  storageBucket: 'chat-app-1c6f3.appspot.com',
+  messagingSenderId: '102670169762',
+  appId: '1:102670169762:web:79b743e6cf2c379c7589fb',
+};
+
+const app = firebase.initializeApp(config);
