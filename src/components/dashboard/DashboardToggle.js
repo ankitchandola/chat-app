f860d0494116e29ddc0b92442a72e3c7a7278844
ankit <sindeadly25@gmail.com>
2021-06-08 13:50:03 +0530

    created rooms

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import { Button, Drawer, Icon } from 'rsuite';
+import React, { useCallback } from 'react';
+import { Button, Drawer, Icon, Alert } from 'rsuite';
 import Dashboard from '.';
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
+import { auth, database } from '../../misc/firebase';
+import { isOfflineForDatabase } from '../../context/profile.context';
 
 const DashboardToggle = () => {
   const { isOpen, close, open } = useModalState();
 
   const isMobile = useMediaQuery('(max-width: 992px)');
+  const onSignOut = useCallback(() => {
+    database
+      .ref(`/status/${auth.currentUser.uid}`)
+      .set(isOfflineForDatabase)
+      .then(() => {
+        auth.signOut();
+        Alert.info('Signed out', 4000);
+        close();
+      })
+      .catch(err => {
+        Alert.error(err.message, 4000);
+      });
+  }, [close]);
 
   return (
     <>
@@ -14,7 +29,7 @@ const DashboardToggle = () => {
         <Icon icon="dashboard" /> Dashboard
       </Button>
       <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
-        <Dashboard />
+        <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </>
   );
